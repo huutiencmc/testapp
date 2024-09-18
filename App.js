c5bb3ebcc7bfd7ff7ade8b6700d09b8f730f1f84
huutiencmc <huutien012004@gmail.com>
2024-09-18 18:00:31 +0700

    bt

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>huutien</Text>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.content}>
+        <Text style={styles.title}>Đăng nhập</Text>
+        <Text style={styles.subtitle}>Nhập số điện thoại</Text>
+        <Text style={styles.instruction}>
+          Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản tại OneHousing
+          Pro
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nhập số điện thoại của bạn"
+          keyboardType="numeric"
+          maxLength={10}
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        />
+        <TouchableOpacity
+          style={[
+            styles.button,
+            phoneNumber.length === 10
+              ? styles.buttonEnabled
+              : styles.buttonDisabled,
+          ]}
+          disabled={phoneNumber.length !== 10}
+        >
+          <Text style={styles.buttonText}>Tiếp tục</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+  },
+  content: {
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "left", // Canh chữ về phía trái màn hình
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginVertical: 5,
+    textAlign: "left",
+    fontWeight: "500",
+  },
+  instruction: {
+    fontSize: 14,
+    color: "gray",
+    marginBottom: 30,
+    textAlign: "left",
+  },
+  input: {
+    height: 50,
+    borderColor: "#ccc",
+    borderBottomWidth: 1,
+    paddingHorizontal: 10,
+    fontSize: 16,
+    marginBottom: 40,
+  },
+  button: {
+    paddingVertical: 15,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  buttonEnabled: {
+    backgroundColor: "#0a84ff",
+  },
+  buttonDisabled: {
+    backgroundColor: "#e0e0e0",
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#fff",
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-![alt](C:\Users\Acer\OneDrive\Hình ảnh\Ảnh chụp màn hình\Ảnh chụp màn hình 2024-09-16 165103.png)
+![alt text](image-2.png)
